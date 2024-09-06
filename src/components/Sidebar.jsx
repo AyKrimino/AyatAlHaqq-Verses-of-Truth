6037e00f55c019ffea4381ddb30737a15c3e2487
AyKrimino <ayoub.krimi14@gmail.com>
2024-09-06 22:28:59 +0100

    dark mode in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -3,10 +3,12 @@ import { getChaptersList } from "../services/GlobalAPI";
 import { useContext, useEffect, useState } from "react";
 import { IoArrowBack } from "react-icons/io5";
 import { ChaptersListContext } from "../context/ChaptersListContext";
+import { ThemeContext } from "../context/ThemeContext";
 
 const Sidebar = () => {
   const [chapters, setChapters] = useState([]);
   const { setChaptersListIsActive } = useContext(ChaptersListContext);
+  const { theme } = useContext(ThemeContext);
 
   useEffect(() => {
     getChapters();
@@ -22,13 +24,25 @@ const Sidebar = () => {
   };
 
   return (
-    <div className="w-[100%] md:w-[25%] h-[90%] p-4 px-8 pb-8 overflow-y-auto overscroll-none scroll-smooth scrollbar-webkit flex-col gap-3 text-white flex bg-[#2D8C7F] border-t border-[#DCF9EB]">
+    <div
+      className={`w-[100%] md:w-[25%] h-[90%] p-4 px-8 pb-8 overflow-y-auto overscroll-none scroll-smooth scrollbar-webkit flex-col gap-3 text-white flex border-t ${
+        theme === "light"
+          ? "bg-[#2D8C7F] border-[#DCF9EB]"
+          : "bg-[#0F5734] border-[#121212]"
+      } `}
+    >
       <div className="flex items-center mb-2">
-        <h3 className="font-english text-2xl lg:text-3xl font-semibold text-[#226B61] flex-1">
+        <h3
+          className={`font-english text-2xl lg:text-3xl font-semibold ${
+            theme === "light" ? "text-[#226B61]" : "text-[#0b4127]"
+          } flex-1`}
+        >
           Chapters
         </h3>
         <IoArrowBack
-          className="text-lg lg:text-xl text-[#226B61] cursor-pointer"
+          className={`text-lg lg:text-xl ${
+            theme === "light" ? "text-[#226B61]" : "text-[#0b4127]"
+          } cursor-pointer`}
           onClick={() => setChaptersListIsActive(false)}
         />
       </div>
--- src/components/SurahItem.jsx
+++ src/components/SurahItem.jsx
@@ -1,6 +1,16 @@
+import { useContext } from "react";
+import { ThemeContext } from "../context/ThemeContext";
+
 const SurahItem = ({ surahNumber, surahEnglishTitle, surahArabicTitle }) => {
+  const { theme } = useContext(ThemeContext);
   return (
-    <div className="flex items-center gap-4 px-2 py-3 bg-[#297870] hover:bg-[#256F67] rounded-lg cursor-pointer group hover:scale-110 transition-all duration-300 ease-in-out pl-4">
+    <div
+      className={`flex items-center gap-4 px-2 py-3 ${
+        theme === "light"
+          ? "bg-[#297870] hover:bg-[#256F67]"
+          : "bg-[#0d4a2c] hover:bg-[#0b4127]"
+      } rounded-lg cursor-pointer group hover:scale-110 transition-all duration-300 ease-in-out pl-4`}
+    >
       <p className="font-english font-bold">{surahNumber}</p>
       <div className="text-xs lg:text-sm space-y-2 font-semibold flex-1 flex flex-col items-center">
         <p className="font-english">{surahEnglishTitle}</p>
