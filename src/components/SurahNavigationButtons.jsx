b84e8da61d7e1ba8bca8c986305b63bbd9384fa8
AyKrimino <ayoub.krimi14@gmail.com>
2024-09-20 22:32:11 +0100

    fix SurahNavigationButtons theme issues

--- src/components/SurahNavigationButtons.jsx
+++ src/components/SurahNavigationButtons.jsx
@@ -12,8 +12,10 @@ const SurahNavigationButtons = () => {
         {id > 1 && (
           <Link
             to={`/surah/${parseInt(id) - 1}`}
-            className={`border-2 w-40 sm:w-48 border-[#2D8C7F] rounded-full py-2 px-1 text-lg font-medium hover:bg-[#2D8C7F] transition-all duration-300 ease-in-out ${
-              theme === "dark" && "border-[#0F5734] hover:bg-[#0F5734]"
+            className={`border-2 w-40 sm:w-48 rounded-full py-2 px-1 text-lg font-medium  transition-all duration-300 ease-in-out ${
+              theme === "dark"
+                ? "border-[#0F5734] hover:bg-[#0F5734]"
+                : "border-[#2D8C7F] hover:bg-[#2D8C7F]"
             } text-center`}
           >
             Previous Surah
@@ -22,8 +24,10 @@ const SurahNavigationButtons = () => {
         {id < 114 && (
           <Link
             to={`/surah/${parseInt(id) + 1}`}
-            className={`border-2 w-40 sm:w-48 border-[#2D8C7F] rounded-full py-2 px-1 text-lg font-medium hover:bg-[#2D8C7F] transition-all duration-300 ease-in-out ${
-              theme === "dark" && "border-[#0F5734] hover:bg-[#0F5734]"
+            className={`border-2 w-40 sm:w-48 rounded-full py-2 px-1 text-lg font-medium transition-all duration-300 ease-in-out ${
+              theme === "dark"
+                ? "border-[#0F5734] hover:bg-[#0F5734]"
+                : "border-[#2D8C7F] hover:bg-[#2D8C7F]"
             } text-center`}
           >
             Next Surah
